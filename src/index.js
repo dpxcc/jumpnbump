8b44ea0fc0242e354b02c2b317c1f848654b1c6c
Cheng Chen <cchen@singlestore.com>
2024-03-27 21:20:05 -0700

    c

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-new Phaser.Game({
-  type: Phaser.WebGL,
-  canvas: canvas,
-  scene,
-  input: {
-    touch: true
-  },
-  plugins,
-  scale: {
-    width: 720,
-    height: 1280,
-    mode: Phaser.Scale.RESIZE
-  }
-});
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const x_canvasWidth = canvas.width = 400;
+const x_canvasHeight = canvas.height = 256;
+
+const level = {};
+level.level = new Image();
+level.level.src = "levels/default/level.png";
+level.mask = new Image();
+level.mask.src = "levels/default/mask.png";
+
+function render() {
+  ctx.clearRect(0, 0, x_canvasWidth, x_canvasHeight);
+  ctx.drawImage(level.level, 0, 0);
+  ctx.drawImage(level.mask, 0, 0);
+  requestAnimationFrame(render);
+}
+render();
